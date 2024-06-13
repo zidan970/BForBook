fa4a0b32053d74f296b7180defaabfff7cb98be2
zidan970 <amdzidan70@gmail.com>
2024-06-13 15:30:52 +0700

    add services package

---
+++ src/main/java/com/alta/e_commerce/services/AuthenticationService.java
@@ -0,0 +1,62 @@
+package com.alta.e_commerce.services;
+
+import com.alta.e_commerce.models.LoginUserDto;
+import com.alta.e_commerce.models.RegisterUserDto;
+import com.alta.e_commerce.entities.User;
+import com.alta.e_commerce.repositories.UserRepository;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class AuthenticationService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthenticationService(
+        UserRepository userRepository,
+        AuthenticationManager authenticationManager,
+        PasswordEncoder passwordEncoder
+    ) {
+        this.authenticationManager = authenticationManager;
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public User signup(RegisterUserDto input) {
+        System.out.println("cek data register user: " + input.getNickname());
+
+        var user = new User()
+            .setUserId(UUID.randomUUID().toString())
+            .setNickname(input.getNickname())
+            .setEmail(input.getEmail())
+            .setPassword(passwordEncoder.encode(input.getPassword()));
+
+        return userRepository.save(user);
+    }
+
+    public User authenticate(LoginUserDto input) {
+        authenticationManager.authenticate(
+            new UsernamePasswordAuthenticationToken(
+                input.getEmail(),
+                input.getPassword()
+            )
+        );
+
+        return userRepository.findByEmail(input.getEmail()).orElseThrow();
+    }
+
+    public List<User> allUsers() {
+        List<User> users = new ArrayList<>();
+
+        userRepository.findAll().forEach(users::add);
+
+        return users;
+    }
+}

66b1eafbace783498eb1afee518fc0e7d26baa83
zidan970 <amdzidan70@gmail.com>
2024-06-14 12:59:18 +0700

    add product entity

---
+++ src/main/java/com/alta/e_commerce/entities/Product.java
@@ -0,0 +1,43 @@
+package com.alta.e_commerce.entities;
+
+import java.util.Date;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "products")
+public class Product {
+    @Id
+    @Column(nullable = false, length = 50)
+    private String productId;
+
+    private String title;
+
+    private String author;
+
+    private String publisher;
+
+    private Date publicationDate;
+
+    private String genre;
+
+    private String language;
+
+    private Integer page;
+
+    private Float price;
+
+    private Integer stock;
+
+    @ManyToOne
+    @JoinColumn(name = "store_id", nullable = false, referencedColumnName = "storeId")
+    private Store store;
+}

d6f811be7391cd7cde0d2da0420a3b7d744801e5
zidan970 <amdzidan70@gmail.com>
2024-06-14 12:56:38 +0700

    add method findByUserUserId

--- src/main/java/com/alta/e_commerce/repositories/StoreRepository.java
+++ src/main/java/com/alta/e_commerce/repositories/StoreRepository.java
@@ -1,11 +1,15 @@
 package com.alta.e_commerce.repositories;
 
 import com.alta.e_commerce.entities.Store;
+
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
+
 
 @Repository
 public interface StoreRepository extends JpaRepository<Store, String> {
-    
+    Optional<Store> findByUserUserId(String userId);
 }
+

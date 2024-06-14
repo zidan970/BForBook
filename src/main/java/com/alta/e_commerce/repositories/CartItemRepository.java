cefbf5b563c48ecac0cb5a850b7fbd75ea37a166
zidan970 <amdzidan70@gmail.com>
2024-06-14 23:04:28 +0700

    add cart item repositories

---
+++ src/main/java/com/alta/e_commerce/repositories/CartItemRepository.java
@@ -0,0 +1,10 @@
+package com.alta.e_commerce.repositories;
+
+import com.alta.e_commerce.entities.CartItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CartItemRepository extends JpaRepository<CartItem, String> {
+    
+}

72a3d040daca331e2b9046a564bc1d32d60fd2f5
zidan970 <amdzidan70@gmail.com>
2024-06-15 09:54:19 +0700

    add order controller

---
+++ src/main/java/com/alta/e_commerce/controllers/OrderController.java
@@ -0,0 +1,53 @@
+package com.alta.e_commerce.controllers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.alta.e_commerce.entities.User;
+import com.alta.e_commerce.models.OrderRequest;
+import com.alta.e_commerce.models.OrderResponse;
+import com.alta.e_commerce.models.WebResponse;
+import com.alta.e_commerce.services.OrderService;
+import com.alta.e_commerce.services.CartService;
+
+@RestController
+public class OrderController {
+    private static final Logger log = LoggerFactory.getLogger(CartItemController.class);
+
+    @Autowired
+    private CartService cartService;
+
+    @Autowired
+    private OrderService orderService;
+
+    @PostMapping(
+            path = "/orders",
+            //consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+
+    public WebResponse<OrderResponse> checkout(@RequestBody OrderRequest request){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        User genzaiNoShiyousha = (User) authentication.getPrincipal();
+        System.out.println("your id: " + genzaiNoShiyousha.getUserId());
+
+        // find cart with current user id! if there is one, then get the cart id!
+        String cartId = cartService.findCartId(genzaiNoShiyousha.getUserId());
+
+        request.setCartId(cartId);
+        System.out.println("id cart: " + request.getCartId());
+
+        OrderResponse orderResponse = orderService.checkout(request);
+        return WebResponse.<OrderResponse>builder()
+                .message("success order")
+                .data(orderResponse)
+                .build();
+    }
+}

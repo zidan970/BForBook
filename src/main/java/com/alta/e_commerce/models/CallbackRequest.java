72806c51711b18490db8faab7baf92997b15456b
zidan970 <amdzidan70@gmail.com>
2024-06-16 08:56:20 +0700

    add callback models

---
+++ src/main/java/com/alta/e_commerce/models/CallbackRequest.java
@@ -0,0 +1,14 @@
+package com.alta.e_commerce.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CallbackRequest {
+    private String orderId;
+}

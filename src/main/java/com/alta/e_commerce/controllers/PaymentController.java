ec624f6edc344dff62063eaf0597333ea5631d02
zidan970 <amdzidan70@gmail.com>
2024-06-16 08:50:50 +0700

    add callback method

--- src/main/java/com/alta/e_commerce/controllers/PaymentController.java
+++ src/main/java/com/alta/e_commerce/controllers/PaymentController.java
@@ -11,7 +11,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.alta.e_commerce.entities.Order;
+import com.alta.e_commerce.entities.Payment;
 import com.alta.e_commerce.entities.User;
+import com.alta.e_commerce.models.CallbackRequest;
 import com.alta.e_commerce.models.PaymentRequest;
 import com.alta.e_commerce.models.PaymentResponse;
 import com.alta.e_commerce.models.WebResponse;
@@ -37,15 +39,14 @@ public class PaymentController {
             consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE
     )
-
-    public WebResponse<PaymentResponse> create(@RequestBody PaymentRequest request){
+    public WebResponse<PaymentResponse> create(@RequestBody PaymentRequest request) {
 
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         User genzaiNoShiyousha = (User) authentication.getPrincipal();
         System.out.println("your id: " + genzaiNoShiyousha.getUserId());
 
         // find cart with current user id! if there is one, then get the cart id!
-        String cartId = cartService.findCartId(genzaiNoShiyousha.getUserId());
+        String cartId = cartService.findOrCreateCart(genzaiNoShiyousha.getUserId());
         System.out.println("your cart id: " + cartId);
 
         // find order with current cart id! if there is one, then get the order id!
@@ -62,4 +63,33 @@ public class PaymentController {
                 .data(paymentResponse)
                 .build();
     }
+
+    @PostMapping(
+            path = "/callback",
+            //consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public WebResponse<String> callback(@RequestBody CallbackRequest request) {
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        User genzaiNoShiyousha = (User) authentication.getPrincipal();
+        System.out.println("your id in callback: " + genzaiNoShiyousha.getUserId());
+
+        // find order with current order id! if there is one, then get it!
+        Order order = orderService.findOrder(request.getOrderId());
+        System.out.println("your order id: " + order.getOrderId());
+
+        Payment payment = paymentService.findPayment(order.getOrderId());
+
+        paymentService.stockChange(order, payment);
+        
+        return WebResponse.<String>builder()
+                .message("your payment is successful")
+                .build();        
+    }
 }
+            
+                                                                                                                         
+                
+                
+                    
